c72310d814fe270a13858667bce0b92f145093e9
Jonnas Pedro <contatojonnaspedro@gmail.com>
2026-03-25 10:39:36 -0300

    Finalizado

--- App.js
+++ App.js
@@ -54,7 +54,6 @@ export default function App() {
             ),
           })}
         />
-
         <Stack.Screen 
           name="CadastroContato" 
           component={CadastroContatoScreen} 
